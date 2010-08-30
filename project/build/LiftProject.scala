1b86aedc14ba79adc19c4e087a2b52174fbd4b5b
Scott Abernethy <scott@pixietwin.com>
2010-08-30 16:46:33 +1200

    Upgrading jetty to fix memory leak issues.

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -9,7 +9,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   override def libraryDependencies = Set(
     "net.liftweb" % "lift-mapper" % liftVersion % "compile->default",
     "net.liftweb" % "lift-wizard" % liftVersion % "compile->default",
-    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.25" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test->default",
     "com.h2database" % "h2" % "1.2.121"
