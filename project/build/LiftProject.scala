0a00702148dc97b76eac34e0d27e049586414651
Scott Abernethy <scott@pixietwin.com>
2010-09-16 10:52:44 +1200

    Changing default db to mysql

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -15,6 +15,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "org.mortbay.jetty" % "jetty" % "6.1.25" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default",
-    "com.h2database" % "h2" % "1.2.138"
+    "com.h2database" % "h2" % "1.2.138",
+    "mysql" % "mysql-connector-java" % "5.1.9"
   ) ++ super.libraryDependencies
 }
