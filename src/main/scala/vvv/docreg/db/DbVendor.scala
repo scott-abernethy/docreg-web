69cb4015890f595970b8915d31b66777d5a38a23
Scott Abernethy <scott@pixietwin.com>
2011-06-29 19:28:18 +1200

    Db vending props adjustment.

--- src/main/scala/vvv/docreg/db/DbVendor.scala
+++ src/main/scala/vvv/docreg/db/DbVendor.scala
@@ -11,8 +11,7 @@ trait DbVendor {
     if (!DB.jndiJdbcConnAvailable_?) {
       val vendor =
 	      new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
-			     Props.get("db.url") openOr
-			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+			     Props.get("db.url") openOr "jdbc:h2:mem:lift_proto.db",
 			     Props.get("db.user"), Props.get("db.password"))
 
       LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
