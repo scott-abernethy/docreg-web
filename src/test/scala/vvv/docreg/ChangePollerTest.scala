4bb3b6e6266ddde81669715a48ed4cb9faf2d0e0
Scott Abernethy <scott@pixietwin.com>
2011-08-23 17:32:29 +1200

    Removing next change function from agent lib

---
+++ src/test/scala/vvv/docreg/ChangePollerTest.scala
@@ -0,0 +1,13 @@
+package vvv.docreg
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sabernethy
+ * Date: 18/08/11
+ * Time: 2:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ChangePollerTest {
+
+}
