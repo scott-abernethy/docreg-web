bf712ff7330dd26f6158ac5d4f296b567bf77a8d
Scott Abernethy <scott@pixietwin.com>
2011-11-21 13:16:31 +1300

    Fixing revision test.

--- src/test/scala/vvv/docreg/model/RevisionTest.scala
+++ src/test/scala/vvv/docreg/model/RevisionTest.scala
@@ -1,21 +1,14 @@
 package vvv.docreg.model
 
 import org.specs._
-import org.specs.runner.JUnit4
-import org.specs.runner.ConsoleRunner
-import org.specs.matcher._
-import org.specs.specification._
 import vvv.docreg.db.TestDbVendor
 import java.util.Date
 
-class RevisionTestSpecsAsTest extends JUnit4(RevisionTestSpecs)
-object RevisionTestSpecsRunner extends ConsoleRunner(RevisionTestSpecs)
-
-object RevisionTestSpecs extends Specification {
+object RevisionTest extends Specification {
   "Revision Model" should {
     "have a full title representing key version title" in {
       val d = Document.create.key("0567").title("Foo bar 2")
-      Revision.create.document(d).version(45).fullTitle must be equalTo("0567-45: Foo bar 2")
+      Revision.create.document(d).version(45).fullTitle must be equalTo("0567: Foo bar 2")
     }
     "not dirty itself when comment set but not changed" >> {
       TestDbVendor.initAndClean()
