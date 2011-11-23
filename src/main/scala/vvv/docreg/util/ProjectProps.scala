912ce1ee40c8426e98189d4eed5489e47ab8b44d
Scott Abernethy <scott@pixietwin.com>
2011-11-24 09:42:32 +1300

    Missed property version up rev

--- src/main/scala/vvv/docreg/util/ProjectProps.scala
+++ src/main/scala/vvv/docreg/util/ProjectProps.scala
@@ -14,7 +14,7 @@ object ProjectProps {
   lazy val props: Properties = {
     val p = new Properties
     p.setProperty("project.name", "DocReg+Web")
-    p.setProperty("project.version", "0.5.3")
+    p.setProperty("project.version", "0.6.0")
     p
   }
 }
