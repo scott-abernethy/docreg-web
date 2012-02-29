e456e8a4d66073dbe3c46095697d5724bf0704d3
Scott Abernethy <scott@pixietwin.com>
2012-02-29 14:24:13 +1300

    Fixing null timeZone field issue. Have to check == null.

--- src/main/scala/vvv/docreg/model/User.scala
+++ src/main/scala/vvv/docreg/model/User.scala
@@ -85,7 +85,7 @@ class User extends LongKeyedMapper[User] with IdPK with ManyToMany {
   }
   
   def getTimeZone(): TimeZone = {
-    if (timeZone.isEmpty){
+    if (timeZone.is == null){
       TimeZone.getDefault
     } else {
       timeZone.isAsTimeZone
