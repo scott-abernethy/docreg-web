6f95d3721b4150d8edb27838816b68ccd39a2163
Scott Abernethy <scott@pixietwin.com>
2012-06-14 16:09:13 +1200

    Major up rev, security release

--- src/main/scala/vvv/docreg/util/ProjectProps.scala
+++ src/main/scala/vvv/docreg/util/ProjectProps.scala
@@ -14,7 +14,7 @@ object ProjectProps {
   lazy val props: Properties = {
     val p = new Properties
     p.setProperty("project.name", "DocReg+Web")
-    p.setProperty("project.version", "0.7.2")
+    p.setProperty("project.version", "0.8.0")
     p
   }
 }
